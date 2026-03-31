7f85a9d3daa7917088295cda1362fc9d09b11043
musayeff222 <lsgrmusazad@gmail.com>
2026-03-31 22:30:18 +0400

    update

--- vite.config.ts
+++ vite.config.ts
@@ -10,6 +10,9 @@ const __dirname = path.dirname(__filename);
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    build: {
+      outDir: 'dist',
+    },
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
