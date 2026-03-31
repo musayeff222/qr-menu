a1a916fd44ae085894fb97bee39155adb110e9ab
musayeff222 <lsgrmusazad@gmail.com>
2026-03-31 22:45:56 +0400

    update

--- server.js
+++ server.js
@@ -1,15 +1,1 @@
-import { spawn } from 'node:child_process';
-import { fileURLToPath } from 'node:url';
-import path from 'node:path';
-
-const dir = path.dirname(fileURLToPath(import.meta.url));
-const child = spawn(
-  process.execPath,
-  ['--import', 'tsx', path.join(dir, 'server.ts')],
-  { stdio: 'inherit', cwd: dir, env: process.env }
-);
-
-child.on('exit', (code, signal) => {
-  if (signal) process.kill(process.pid, signal);
-  process.exit(code ?? 1);
-});
+import './dist-server/server.js';
